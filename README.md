b0c54de3a1951ac1699dcb9171ac851bd028a427
Tobias Davis <public@davistobias.com>
2012-12-05 02:03:20 -0600

    Finalized first version of parser functionality (not convert-to-markdown/etc.), added readme.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ wordpress-export
 ================
 
 Exporting all your WordPress posts to Markdown with metadata.
+
+Steps to success:
+
+1. Go to your WordPress website, probably log in as the admin
+2. Go to `Tools` > `Export` and export all posts and pages, this gives you an XML file
+3. Install this Node code with `npm install wordpress-export`
+4. Run this with `node wordpress-export.js /path/to/export.xml`
+5. Look in the folder `wordpress-export` for your Markdown files *and* media files
+
+Now you can run something like [Noddity](http://noddity.com) to run your blog!
--- wordpress-export.js
+++ wordpress-export.js
@@ -10,9 +10,7 @@ var settings = {
 fs.readFile(settings.fileName, settings.fileEncoding, function (err, data) {
 	if (!err) {
 		parser.parseString(data, function (err, result) {
-			for (var i = result.rss.channel.length - 1; i >= 0; i--) {
-				parseChannel(result.rss.channel[i])
-			}
+			createFiles(parseChannel(result.rss.channel[0]))
 			console.log('Done')
 		})
 	} else {
@@ -21,5 +19,81 @@ fs.readFile(settings.fileName, settings.fileEncoding, function (err, data) {
 })
 
 var parseChannel = function(channel) {
-	console.dir(channel['wp:wxr_version'])
+	var blog = {}
+
+	// set the singletons
+	blog.title = channel.title
+	blog.link = channel.link
+	blog.description = channel.description
+	blog.language = channel.language
+
+	// set authors
+	var wp_authors = channel['wp:author']
+	var authors = []
+	for (var i = 0; i < wp_authors.length; i++) {
+		var wp_author = {}
+		wp_author.id = wp_authors[i]['wp:author_id'][0]
+		wp_author.login = wp_authors[i]['wp:author_login'][0]
+		wp_author.email = wp_authors[i]['wp:author_email'][0]
+		wp_author.display_name = wp_authors[i]['wp:author_display_name'][0]
+		// if CDATA is empty it will be an object, otherwise it will be a string
+		wp_author.first_name = (typeof wp_authors[i]['wp:author_first_name'][0] === 'object' ? null : wp_authors[i]['wp:author_first_name'][0])
+		wp_author.last_name = (typeof wp_authors[i]['wp:author_last_name'][0] === 'object' ? null : wp_authors[i]['wp:author_last_name'][0])
+		authors.push(wp_author)
+	}
+	blog.authors = authors
+
+	// parse posts
+	var posts = []
+	for (var i = 0; i < channel.item.length; i++) {
+		console.log('Parsing post ' + (i + 1) + ' of ' + channel.item.length)
+		posts.push(parsePost(channel.item[i]))
+	}
+	blog.posts = posts
+
+	return blog
+}
+
+var parsePost = function(wp_post) {
+	// singletons
+	var post = {}
+	post.title = wp_post.title[0]
+	post.link = wp_post.link[0] // strip domain
+	post.published_date = wp_post['wp:post_date_gmt'][0] // convert to JS date
+	post.author = wp_post['dc:creator'][0] // grab from the blog.authors list
+	post.permalink = wp_post.guid[0]._
+	post.content = (typeof wp_post['content:encoded'][0] === 'object' ? null : wp_post['content:encoded'][0])
+	post.excerpt = (typeof wp_post['excerpt:encoded'][0] === 'object' ? null : wp_post['excerpt:encoded'][0])
+	post.id = Number(wp_post['wp:post_id'][0])
+	post.allow_comments = (wp_post['wp:comment_status'][0] === 'open' ? true : false)
+	post.allow_pings = (wp_post['wp:ping_status'][0] === 'open' ? true : false)
+	post.slug = wp_post['wp:post_name'][0]
+	post.published = (wp_post['wp:status'][0] === 'publish' ? true : false)
+	post.is_post = (wp_post['wp:post_type'][0] === 'post' ? true : false)
+
+	// categories and tags are like [{url_safe, print_name}]
+	var categories = []
+	var tags = []
+	if(typeof wp_post.category !== 'undefined') {
+		for (var i = 0; i < wp_post.category.length; i++) {
+			var values = wp_post.category[i].$
+			var obj = { url_safe : values.nicename, print_name : wp_post.category[i]._ }
+			if (values.domain === 'category') {
+				categories.push(obj)
+			}
+			else if (values.domain === 'post_tag') {
+				tags.push(obj)
+			}
+		}
+	}
+	post.categories = categories
+	post.tags = tags
+
+	return post
+}
+
+var createFiles = function(blog) {
+	for (var i = 0; i < blog.posts.length; i++) {
+		console.log(blog.posts[i].title)
+	};
 }
